cb0441583d97515714aaade6abf5af91fffa7cbc
David Barnes <dbarnes@kvcc.edu>
2016-04-17 13:07:07 -0400

    Initial commit.

---
+++ cis237inclass6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cis237inclass6.Startup))]
+namespace cis237inclass6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
